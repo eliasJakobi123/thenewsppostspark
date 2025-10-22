7840b6e8d99f2520904329aa0af51bb066988c35
eliasJakobi123 <elias6weiser@gmail.com>
2025-10-22 18:05:20 +0200

    Add environment variable support for Vercel deployment

---
+++ set-env-vars.js
@@ -0,0 +1,7 @@
+// Set Environment Variables for local testing
+window.VITE_SUPABASE_URL = 'https://ntutkssgqzqgmbvuwjqu.supabase.co';
+window.VITE_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dXRrc3NncXpxZ21idnV3anF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NzIwODcsImV4cCI6MjA3NjI0ODA4N30.7sVEt76VK0INektXVqB5xsDnfQolW7Bzz0MeJ63CevE';
+window.VITE_REDDIT_CLIENT_ID = 'xnfBBEUETLqctZnhAka0DA';
+window.VITE_REDDIT_CLIENT_SECRET = 'uLXMyoHsE8uQyZGhYW3ZMpbJ65BdHA';
+window.VITE_REDDIT_REDIRECT_URI = 'http://localhost:8080';
+window.VITE_OPENAI_API_KEY = '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';
